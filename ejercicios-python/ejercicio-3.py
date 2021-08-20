ab267d669f1b1da33b487638beeda6769dfd325b
Patrick Vilchez <pvilchez794@gmail.com>
2021-08-20 13:03:46 -0500

    Primera parde de la barra de navegacion

--- ejercicios-python/ejercicio-3.py
+++ ejercicios-python/ejercicio-3.py
@@ -1,6 +1,5 @@
 # Multiplicar dos números sin utilizar el símbolo de multiplicación
 
-
 a = 4
 b = 8
 resultado = 0
@@ -10,8 +9,8 @@ for x in range(a):
 
 print(resultado)
 
-# Ingresar un nombre y apellido e imprimirlo al reves
 
+# Ingresar un nombre y apellido e imprimirlo al reves
 
 nombre = 'Patrick'
 apellido = 'Feliz'
@@ -20,8 +19,8 @@ concatencacion = nombre + ' ' + apellido
 
 print(concatencacion[::-1])
 
-# Escribi una función que encuentre el elemento menor de una lista
 
+# Escribi una función que encuentre el elemento menor de una lista
 
 lista = [1, 2, 5, 3, 55, -24, -13]
 
@@ -35,9 +34,9 @@ for x in lista:
 
 print(menor)
 
+
 # Escribir una función que devuelva el volumen de una esfera por su radio
 # (4/3 * pi ** 3)
-
 
 def calcula_volumen(r):
     return 4 / 3 * 3.14 * r ** 3
@@ -47,8 +46,8 @@ resultado = calcula_volumen(6)
 
 print(resultado)
 
-# Escribir una función que indique si el usuario es mayor de edad
 
+# Escribir una función que indique si el usuario es mayor de edad
 
 def es_mayor(usuario):
     return usuario.edad > 17
@@ -67,8 +66,8 @@ resultado2 = es_mayor(usuario2)
 
 print(resultado1, resultado2)
 
-# Escribir una función que indique si un número es par o impar
 
+# Escribir una función que indique si un número es par o impar
 
 def es_par(n):
     return n % 2 == 0
@@ -90,9 +89,9 @@ for x in palabra:
 
 print(vocales)
 
+
 # Escribir una aplicación que reciba una cantidad infinita de números
 # hasta decir basta, luego que devuelva la suma de los números ingresados
-
 
 lista = []
 
